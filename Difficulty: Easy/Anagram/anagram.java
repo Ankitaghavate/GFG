ff53baa4ce6397eb60f636a237f339227b47a37f
Ankita Ghavate <144261424+Ankitaghavate@users.noreply.github.com>
2026-08-13 00:17:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        // code here
+        HashMap<Character,Integer> map=new HashMap<>();
+        if(s1.length()!=s2.length())
+        {
+            return false;
+        }
+        
+        for(int i=0;i<s1.length();i++)
+        {
+            char ch=s1.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        
+        for(int i=0;i<s2.length();i++)
+        {
+            char ch=s2.charAt(i);
+            
+            if(map.get(ch)!=null)
+            {
+                if(map.get(ch)==1)
+                {
+                    map.remove(ch);
+                }
+                else
+                {
+                    map.put(ch,map.get(ch)-1);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return map.isEmpty();
+    }
+}
